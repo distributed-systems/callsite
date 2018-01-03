05fbf8dc19200eea716d3a5fd9c62c8cad16b5fd
Michael van der Weg <michael@joinbox.com>
2018-01-03 22:47:03 +0100

    feat: implement initial functionality

---
+++ src/Callsite.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const assert = require('assert');
+
+
+
+
+module.exports = class CallSite {
+
+
+
+    /**
+    * returns a serializable frame
+    *
+    * @param {object} frame v8 callsit object
+    *
+    * @returns {object} frame
+    */
+    convertStackFrame(frame) {
+        assert(typeof frame === 'object', 'the callsite must be an object');
+
+        const callsite = {};
+
+        callsite.type          = frame.getTypeName();
+        callsite.function      = frame.getFunctionName() || '<anonymous>';
+        callsite.method        = frame.getMethodName();
+        callsite.fileName      = frame.getFileName();
+        callsite.lineNumber    = frame.getLineNumber();
+        callsite.message       = frame.toString();
+
+        return callsite;
+    }
+
+
+
+
+
+
+
+    /**
+    * return json stack frames
+    *
+    * @returns {array} stack
+    */
+    getStack({
+        slice,
+        limit,
+        err,
+        fn = this.getStack,
+    } = {}) {
+        const frames = this.getRawStack({slice, limit, err, fn});
+        return frames.map(frame => this.convertStackFrame(frame))
+    }
+
+
+
+
+
+
+
+    /**
+    * return raw stack frames
+    *
+    * @returns {array} stack
+    */
+    getRawStack({
+        slice = 0,
+        limit = 20,
+        fn = this.getRawStack,
+        err = new Error(),
+    } = {}) {
+        const originalFunction = Error.prepareStackTrace;
+        const originalLimit = Error.stackTraceLimit;
+
+        Error.stackTraceLimit = limit;
+        Error.prepareStackTrace = (originalFunction, stack) => stack;
+
+        // caputre from a cerrtain offset
+        Error.captureStackTrace(err, fn);
+
+        // revert
+        Error.prepareStackTrace = originalFunction;
+        Error.stackTraceLimit = originalLimit;
+        
+        // return the raw stack
+        return err.stack.slice(slice);
+    }
+};
